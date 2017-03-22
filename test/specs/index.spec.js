b9357ee338c6f1247b8dd850bc467475e30e0ebe
Rémi Alvergnat <toilal.dev@gmail.com>
2017-03-22 16:24:13 +0100

    Add support for sourcemap (#5)
    * Add support for sourcemap
    
    Close #4
    
    * Add support for loader sourceMap option and generate sourcemap from scratch when previous sourcemap is undefined
    
    * Avoid defining location/sourceFile parse options when sourceMap is falsy

--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -1,13 +1,18 @@
 const path = require('path')
 const fs = require('fs')
-const loader = require('../../lib/index')
+const DefaultLoader = require('../../lib/index')
+
+class Loader extends DefaultLoader {
+  callback(err, contents, sourcemap) { this.callbackAnswer = { err, contents, sourcemap } }
+}
 
 function test (name) {
   const src = fs.readFileSync(path.resolve(__dirname, '../fixtures', name), 'utf8')
   const expected = fs.readFileSync(path.resolve(__dirname, '../expects', name), 'utf8')
 
   it(name, () => {
-    expect(loader(src).replace(/if \(module\.hot\)[\s\S]*$/, '').trim()).toBe(expected.trim())
+    const loader = new Loader(src)
+    expect(loader.callbackAnswer.contents.replace(/if \(module\.hot\)[\s\S]*$/, '').trim()).toBe(expected.trim())
   })
 }
 
