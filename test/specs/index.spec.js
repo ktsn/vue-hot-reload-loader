0ae2b0da48cb4f1ac17473d299c86149e01a1824
ktsn <ktsn55@gmail.com>
2018-09-23 10:32:01 +0900

    chore: bump deps

--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -5,24 +5,35 @@ const loader = require('../../lib/index')
 function loadCode(data) {
   let callbackAnswer
 
-  loader.call({
-    resourcePath: '/path/to/test.html',
-    options: {},
-    callback: (err, contents, sourcemap) => {
-      callbackAnswer = { err, contents, sourcemap }
-    }
-  }, data)
+  loader.call(
+    {
+      resourcePath: '/path/to/test.html',
+      options: {},
+      callback: (err, contents, sourcemap) => {
+        callbackAnswer = { err, contents, sourcemap }
+      }
+    },
+    data
+  )
 
   return callbackAnswer
 }
 
-function test (name) {
-  const src = fs.readFileSync(path.resolve(__dirname, '../fixtures', name), 'utf8')
-  const expected = fs.readFileSync(path.resolve(__dirname, '../expects', name), 'utf8')
+function test(name) {
+  const src = fs.readFileSync(
+    path.resolve(__dirname, '../fixtures', name),
+    'utf8'
+  )
+  const expected = fs.readFileSync(
+    path.resolve(__dirname, '../expects', name),
+    'utf8'
+  )
 
   it(name, () => {
     const callbackAnswer = loadCode(src)
-    expect(callbackAnswer.contents.replace(/if \(module\.hot\)[\s\S]*$/, '').trim()).toBe(expected.trim())
+    expect(
+      callbackAnswer.contents.replace(/if \(module\.hot\)[\s\S]*$/, '').trim()
+    ).toBe(expected.trim())
   })
 }
 
